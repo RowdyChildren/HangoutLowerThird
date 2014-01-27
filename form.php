63c8c17d3dcee0c682caa10d50c0a682ca0958a9
Riley Childs <Riley@TFSGEO.com>
2014-01-26 21:32:00 -0500

    Added header to form.php
    Added header to form.php

--- form.php
+++ form.php
@@ -1,4 +1,5 @@
 <!-- This is a Generic form to create the third, Note the field names! -->
+<h1>Google Plus Lower Third Maker</h1><br>
 <form method='post' action='image.php'>
 Name: <input type='text' name='name' value='Put Your First/Last Name Here!' length='60' /><br>
 Title: <input type='text' name='sub' value='Put Your Title Here!' length='60' /><br>
